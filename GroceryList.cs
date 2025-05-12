310684886b3bf2e4a68d2bd46f5987efb107371c
AbdulWasaybinHassaan <universityworkonly01@gmail.com>
2025-05-12 23:56:30 +0500

    Create GroceryList.cs

---
+++ GroceryList.cs
@@ -0,0 +1,8 @@
+using System;
+  Console.WriteLine("Grocery Items:\n ");
+  Console.WriteLine("Detergent Powder");
+  Console.WriteLine("Spaghetti");
+  Console.WriteLine("Cooking Oil");
+  Console.WriteLine("Astonish Car Care: Engine Degreaser");
+  Console.WriteLine("Sugar");
+  Console.WriteLine("Table Salt");
